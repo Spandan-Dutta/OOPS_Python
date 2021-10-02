205535220bfbf443e2d6dc68f2457a1597b25226
Spandan Dutta <67653554+Spandan-Dutta@users.noreply.github.com>
2021-10-02 14:39:43 +0530

    Create Inheritence_Example.py

---
+++ Inheritence_Example.py
@@ -0,0 +1,15 @@
+# Code example of Inheritence:
+class Animal:
+    def speak(self):
+        print("Animal Speaking")
+class Dog(Animal):
+    def bark(self):
+        print("Dog Barks")
+class DogChild(Dog):
+    def eat(self):
+        print("Eating bread...")
+
+d = DogChild()
+d.bark()
+d.speak()
+d.eat()
